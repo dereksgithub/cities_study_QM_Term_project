ea02a757736fc63c4e0f83bfeb59cfb39f14e22d
Derek Ding <derekding727@gmail.com>
2024-01-20 17:44:42 +0000

    merged with another dataset to see the diffference

--- dataprocessing/county_matcher.py
+++ dataprocessing/county_matcher.py
@@ -1,6 +1,25 @@
 # match FIPS code with county name
-
+import re
 # input: FIPS code
+
+def fips_match(stringA, stringB):
+    # Splitting the strings at "|"
+    leftA, rightA = stringA.lower().split("|")
+    leftB, rightB = stringB.lower().split("|")
+
+    # Check exact match on the left
+    if leftA != leftB:
+        return False
+
+    # Check if rightB matches the first 2 to 3 words of rightA
+    match_pattern = r'^(' + re.escape(rightB) + r')(\s+\w+){0,2}'
+    return bool(re.match(match_pattern, rightA))
+
+
+# Example usage
+stringA = "AK | ALEUTIANS EAST BOROUGH"
+stringB = "AK | Aleutians East"
+print(fips_match(stringA, stringB)) 
 
 
 # output: county name
